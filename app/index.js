e49506fb6e5ca78790f610b7c098ffaffa38cbb6
Nguyen Phuc <phuc.nguyen@mail.rakuten.com>
2016-05-18 10:00:33 +0900

    Implement redux

--- app/index.js
+++ app/index.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import createStore from './stores';
 import App from './components/app';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const store = createStore();
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('app')
+);
