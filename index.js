2035d5b5f76a44b9da563a8f6c913e57d4dd75f5
Nguyen Hoang Phuc <phucnguyenhoang1985@gmail.com>
2017-02-19 22:41:32 +0900

    replace ejs with pug

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ const app = express();
 const compression = require('compression')
 
 app.use(compression());
-app.set('port', (process.env.PORT || 3000));
+app.set('port', (process.env.PORT || 4000));
 app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/libs'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.set('view engine', 'pug');
 
 // New change to support using browserHistory (react-router);
 app.get('*', function (request, response){
